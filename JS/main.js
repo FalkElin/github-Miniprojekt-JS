dc6ec5de8301f4933105b563fe2bec750db4bd93
Elin Falk <eliin_falk@live.se>
2021-12-11 10:39:36 +0100

    welcome widget frist setup Fixes #3

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,5 @@
 window.addEventListener("load", main);
 
 function main() {
-  addEventListeners();
-  startCalender();
-}
-
-function addEventListeners() {
-  const previousMonthBtn = document.getElementById("previousMonthBtn");
-  const nextMonthButton = document.getElementById("nextMonthBtn");
-
-  previousMonthBtn.addEventListener("click", previous);
-  nextMonthButton.addEventListener("click", next);
-
-  const changeInCalender = document.getElementById("month");
-
-  changeInCalender.addEventListener("change", jump);
+  getLocation();
 }
