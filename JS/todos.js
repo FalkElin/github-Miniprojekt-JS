becc912d5de8d2371c8f7e530d0ce5d4e7820277
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2021-12-13 14:28:23 +0100

    commit

--- JS/todos.js
+++ JS/todos.js
@@ -1,91 +1,95 @@
-let todos = [{title: 'Plugga', date: 2021-12-12, color: 'red'}, {title: 'Tvätta', date: 2021-12-14, color: 'blue'}, {title: 'Köpa julklappar', date: 2021-12-10, color: 'yellow'}];
+const todoObjectList = [];
 
-//titel, date, color i arrayen;
-//colorPicker.addEventListener('input', updateFirst, false);
-//colorPicker.addEventListener('change', watchColorPicker, false);
 
-//function watchColorPicker(event) {
-//  document.querySelectorAll('p').forEach(function(p) {
-//    p.style.color = event.target.value;
-//  });
-//}
+class Todo_Class {
+    constructor(item){
+        this.ulElement =item;
+    } 
 
-let color;
-let defaultColor = "#0000ff";
+    add() {
+        const todoInput = document.querySelector("#myInput").value;
+        if (todoInput == "") {
+            alert("You did not enter any todo") 
+        } else {
+            const todoObject = {
+                id : todoObjectList.length,
+                todoText : todoInput,
+                isDone : false,
+            }
 
-function todo() {
-  renderTodos();
-  const form = document.querySelector("form");
-  //form.addEventListener("submit", addTodo);
-}
+        todoObjectList.unshift(todoObject);
+        this.display();
+        document.querySelector("#myInput").value = '';
 
-function assignColor() {
-  color = document.querySelector("#color");
-  color.value = defaultColor;
-  //color.addEventListener("input", updateFirst, false);
-  //color.addEventListener("change", updateAll, false);
-}
-
-function updateFirst(event) {
-  let changeBackgroundColor = document.querySelector("#color");
-  if (changeBackgroundColor) {
-    changeBackgroundColor.style.backgroundColor = event.target.value;
-  }
-}
-
-function updateAll(event) {
-  document.querySelectorAll("input").forEach(function(input) {
-    input.style.color = event.target.value;
-  });
-}
-
-function getDate() {
-  let date = form.getElementById('#date').value;
-  form.getElementById('#addBtn').innerHTML = date;
-  console.log(date);
-}
-
-function addTodo(event) {
-  event.preventDefault();
-  const form = event.target;
-  const formData = new FormData(form);
-  const todo = Object.fromEntries(formData);
-  console.log(todo);
-
-  //if (input.value) {
-  todos.push(todo);
-  // input.value = "";
-  renderTodos();
-}
-
-function renderTodos() {
-  const ul = document.querySelector("ul");
-  // Tar bort tidigare tillagda todos
-  ul.innerHTML = "";
-  // Lägger till nya todos till listan
-  for (const todo of todos) {
-    const li = createTodoElement(todo);
-    ul.appendChild(li);
-  }
-}
-
-function createTodoElement(todo) {
-  const li = document.createElement("li");
-  li.innerHTML = todo;
-  li.id = todo;
-  //li.addEventListener("click", removeTodo);
-  return li;
-}
-
-function removeTodo(event) {
-  const todoToDelete = event.target.id;
-
-  const updatedTodoList = [];
-  for (const todo of todos) {
-    if (todo !== todoToDelete) {
-      updatedTodoList.push(todo);
+        }
     }
-  }
-  todos = updatedTodoList;
-  renderTodos();
-}
+
+    done_undone(x) {
+        const selectedTodoIndex = todoObjectList.findIndex((item)=> item.id == x);
+        console.log(todoObjectList[selectedTodoIndex].isDone);
+        todoObjectList[selectedTodoIndex].isDone == false ? todoObjectList[selectedTodoIndex].isDone = true : todoObjectList[selectedTodoIndex].isDone = false;
+        this.display();
+    }
+
+    deleteElement(z) {
+        const selectedDelIndex = todoObjectList.findIndex((item)=> item.id == z);
+
+        todoObjectList.splice(selectedDelIndex,1);
+
+        this.display();
+    }
+
+
+    display() {
+        this.ulElement.innerHTML = "";
+
+        todoObjectList.forEach((object_item) => {
+
+            const liElement = document.createElement("li");
+            const delBtn = document.createElement("i");
+
+            liElement.innerText = object_item.todoText;
+            liElement.setAttribute("data-id", object_item.id);
+
+            delBtn.setAttribute("data-id", object_item.id);
+            delBtn.classList.add("far", "fa-trash-alt");
+
+            liElement.appendChild(delBtn);
+            
+            delBtn.addEventListener("click", function(e) {
+                const deleteId = e.target.getAttribute("data-id");
+                myTodoList.deleteElement(deleteId);
+            })
+            
+            liElement.addEventListener("click", function(e) {
+                const selectedId = e.target.getAttribute("data-id");
+                myTodoList.done_undone(selectedId);
+            })
+
+            if (object_item.isDone) {
+                liElement.classList.add("checked");
+            }
+
+            this.ulElement.appendChild(liElement);
+        })
+    }
+} 
+
+
+
+
+//----MAIN PROGRAM------------
+const listSection = document.querySelector("#myUL");
+
+myTodoList = new Todo_Class(listSection);
+
+
+document.querySelector(".addBtn").addEventListener("click", function() {
+    myTodoList.add()
+})
+
+document.querySelector("#myInput").addEventListener("keydown", function(e) {
+    if (e.keyCode == 13) {
+        myTodoList.add()
+    }
+})
