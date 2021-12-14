6094cf75121958f47f31d0812b0fb9516b087184
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2021-12-14 16:03:42 +0100

    now possible to remove a todo #18

--- JS/todos.js
+++ JS/todos.js
@@ -62,14 +62,3 @@ function removeTodo(event) {
   renderTodos();
 }
 
-function changeTodo() {
-const input = document.querySelector('input');
-const log = document.getElementById('log');
-
-input.addEventListener('change', updateValue);
-
-function updateValue(e) {
-  log.textContent = e.target.value;
-}
-
-}
