c5890b0322e152279dd33480cc2263efa82b4eac
linnea <linnea.albertsson@gmail.com>
2021-12-14 15:51:27 +0100

    kalender head fungerar

--- JS/newcalendar.js
+++ JS/newcalendar.js
@@ -2,9 +2,6 @@ window.onload = main();
 
 function main() {
     addCalendarEventlisteners();
-    // renderCalendar();
-}
-function addCalendarEventlisteners() {
 }
 
 let calendar = {
@@ -13,14 +10,11 @@ let calendar = {
     year: null,
     today: null
 }
+
 calendar.month = calendar.date.getMonth();
-// console.log(calendar.month);
-console.log(calendar.date.getDay());
 calendar.year = calendar.date.getFullYear();
 calendar.today = calendar.date.getDay();
-
-
-
+let todaysDate = calendar.date.getUTCDate()
 
 const months = [
     "Januari",
@@ -38,56 +32,76 @@ const months = [
   ];
 
   const weekdays = [
-      'Sön',
-      'Mån',
-      'Tis',
-      'Ons',
-      'Tor',
-      'Fre',
-      'Lör'
+      'Söndag',
+      'Måndag',
+      'Tisdag',
+      'Onsdag',
+      'Tordag',
+      'Fredag',
+      'Lördag'
   ]
+
+function addCalendarEventlisteners() {
+    document.getElementById('arrow-back').addEventListener('click', changeMonthBack);
+
+    document.getElementById('arrow-forward').addEventListener('click', changeMonthForward);
+}
 
 renderCalendar();
 
 function renderCalendar() {
     renderCells();
     renderHeader();
-
+    console.log(calendar.date.getDate());
 
     /**-----Header---- */
     function renderHeader() {
-        document.getElementById('calendarHeader').innerText = months[calendar.month];
-        document.querySelector('.today-text').innerHTML = weekdays[calendar.today] + " " + calendar.year;
+        document.getElementById('calendarHeader').innerText = months[calendar.month] + " " +  + calendar.year;
+        document.querySelector('.today-text').innerHTML = weekdays[calendar.today] + " " + todaysDate;
     }
-
-    
-
 
     /**------Calendar body------- */
-    
-    
     function renderCells() {
         let dateCells = document.querySelectorAll('div.date-cells');
-
+        let daysInMonth = 32 - new Date(calendar.year, calendar.month, 32).getDate();
+        
         /** Clears each cell of text */
         for (let cell of dateCells) {
-        cell.innerHTML = '';
+            cell.innerHTML = '';
         }
-         /** Fill cells with correct date number */
-        for (let i = 1; i <= 31; i++) {
-        dateCells[i].innerHTML= i;
+
+         /** Fill cells with correct date number and adds color to todays date*/
+        for (let i = 1; i <= daysInMonth; i++) {
+            dateCells[i].innerHTML= i;
+
+            if (dateCells[i].innerHTML == todaysDate) {
+                dateCells[i].classList.add('today');
+            }
         }
-         /** Adds background-color to todays date */
-        i=1;
-        if (dateCells[i].innerHTML == calendar.today) {
-        dateCells[i].classList.add('today');
+       
     }
+     
+}
+function changeMonthBack() {
+    if (calendar.month === 0) {
+        calendar.month = 11;
+        calendar.year--;
+        renderCalendar();
+    } else {
+        calendar.month--;
+        renderCalendar();
     }
-  
-
-   
-
-   
+}
+function changeMonthForward() {
+    if (calendar.month === 11) {
+        calendar.month = 0;
+        calendar.year++;
+        renderCalendar();
+    } else {
+        calendar.month++;
+        renderCalendar();
+    }
+}
 
 // decembers nr 1 i kalendern === decembers 1a datum. 
 // 
@@ -101,8 +115,3 @@ function renderCalendar() {
 
 
 
-console.log(calendar.year);
-console.log(calendar.month);
-
-console.log(32 - new Date(calendar.year, calendar.month, 32).getDate());
-}
