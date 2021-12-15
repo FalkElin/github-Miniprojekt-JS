028704cbd99cb1276831a43674c147e7d2a56250
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2021-12-15 15:16:26 +0100

    testar

--- JS/thisweek.js
+++ JS/thisweek.js
@@ -10,6 +10,12 @@ let lat;
 let long;
 
 async function getCurrentWeather(position) {
+<<<<<<< Updated upstream
+=======
+  // gets lon and lat position which is read in api call and shows degree icon
+  const degree = document.querySelector(".temperature");
+  degree.classList.remove("hide-degree");
+>>>>>>> Stashed changes
   lat = position.coords.latitude.toFixed(2);
   long = position.coords.longitude.toFixed(2);
 
@@ -19,7 +25,25 @@ async function getCurrentWeather(position) {
 
   const data = await response.json();
   const temperature = data.main.temp;
+<<<<<<< Updated upstream
 
+=======
+  const sky = data.weather[0].main;
+  const city = data.name;
+
+  //starts functions when api is succesfully returned
+  printCityName(city);
+  printWeatherMessage(temperature);
+  printWeatherIcon(sky);
+}
+
+/**
+ * prints different message depending on current temperature and weather
+ * @param {*} temperature
+ * @param {*} sky
+ */
+function printWeatherMessage(temperature, sky) {
+>>>>>>> Stashed changes
   document.querySelector(".temperature-degree").innerHTML =
     Math.floor(temperature);
 
@@ -32,3 +56,128 @@ async function getCurrentWeather(position) {
       "It's good weather";
   }
 }
+<<<<<<< Updated upstream
+=======
+
+/**
+ * Prints different icon depending on current weather from api
+ * @param {*} sky
+ */
+function printWeatherIcon(sky) {
+  cloud = document.querySelector(".cloud");
+  sun = document.querySelector(".sun");
+  snow = document.querySelector(".snow");
+  wind = document.querySelector(".wind");
+  rain = document.querySelector(".rain");
+  thunder = document.querySelector(".thunder");
+  drizzle = document.querySelector(".drizzle");
+
+  switch (sky) {
+    case "Clouds":
+      cloud.classList.remove("hide-icon");
+      break;
+    case "Rain":
+      rain.classList.remove("hide-icon");
+      break;
+
+    case "Clear":
+      sun.classList.remove("hide-icon");
+      break;
+
+    case "Snow":
+      rain.classList.remove("hide-icon");
+      break;
+
+    case "Thunderstorm":
+      thunder.classList.remove("hide-icon");
+      break;
+
+    case "Drizzle":
+      drizzle.classList.remove("hide-icon");
+      break;
+  }
+}
+
+/**
+ * updates time every minute by calling function that gets current time
+ */
+function startClock() {
+  renderClock();
+  setInterval(renderClock, 6000);
+}
+
+/**
+ *  prints out day of week and date in number
+ */
+function renderClock() {
+  let today = new Date();
+
+  const timeElement = document.querySelector(".location-timezone");
+  timeElement.innerText = getCurrentTime(today);
+
+  const weekdayElement = document.querySelector(".weekday");
+  weekdayElement.innerText =
+    getCurrentWeekday(today) + " " + getDateInNumbers(today);
+}
+
+/**
+ * Gets current time and sends back for printing in renderclock
+ * @param {*} today
+ * @returns hours + minutes
+ */
+function getCurrentTime(today) {
+  let hours = today.getHours();
+  let minutes = today.getMinutes();
+
+  if (hours < 10) hours = "0" + hours;
+  if (minutes < 10) minutes = "0" + minutes;
+
+  return hours + ":" + minutes;
+}
+
+/**
+ * coverts numbers of weekday to swedish weekdays in letters
+ * @param {*} today
+ * @returns days in letters instead of numbers
+ */
+function getCurrentWeekday(today) {
+  const weekday = today.getDay();
+
+  switch (weekday) {
+    case 0:
+      return "Söndag";
+    case 1:
+      return "Måndag";
+    case 2:
+      return "Tisdag";
+    case 3:
+      return "Onsdag";
+    case 4:
+      return "Torsdag";
+    case 5:
+      return "Fredag";
+    case 6:
+      return "Lördag";
+  }
+}
+
+/**
+ * gets current day and month in numbers, adds 1 to month for correct number
+ * @param {*} today
+ * @returns current day + month in numbers
+ */
+function getDateInNumbers(today) {
+  day = today.getDate();
+  month = today.getMonth() + 1;
+
+  return day + "/" + month;
+}
+
+/**
+ * prints current city into div element
+ * @param {*} city
+ */
+function printCityName(city) {
+  document.querySelector(".city").innerHTML = city;
+}
+>>>>>>> Stashed changes
